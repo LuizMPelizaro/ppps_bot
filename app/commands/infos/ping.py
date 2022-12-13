c3ef28495e0052d7d10d92657f666989d8af3f0a
Vinicius Santos <viniciuscarvalhomine@gmail.com>
2022-12-12 23:59:55 -0300

    Adding comments line

--- app/commands/infos/ping.py
+++ app/commands/infos/ping.py
@@ -10,10 +10,13 @@ class Ping(commands.Cog):
 
         @bot.tree.command(name="ping", description="Shows the bot's latency!")
         async def ping(interaction: discord.Interaction):
+            # Making the latency in milliseconds and converting to int.
             bot_latency: int = math.floor((bot.latency * 1000))
+            # Replying the bot's latency only to the sender user (ephemeral param)
             await interaction.response.send_message(f":ping_pong: **Bot's latency is `{bot_latency}ms`**",
                                                     ephemeral=True)
 
 
 async def setup(self):
+    # Adding the class as a slash command
     await self.add_cog(Ping(self))
--- main.py
+++ main.py
@@ -19,18 +19,25 @@ client = discord.Client(intents=intents)
 @bot.event
 async def on_ready():
     print(f"{get_current_time()} :: Logged in as {bot.user}!")
+
+    # Syncing the slash commands to all guilds
     synced = await bot.tree.sync()
     print(f"{get_current_time()} :: Synced {len(synced)} slash commands!")
 
 
 async def load():
+    # Listing all files inside `app/commands/infos`
     for file_name in os.listdir("./app/commands/infos"):
+        # Checking if it is a python file
         if file_name.endswith(".py"):
+            # Registering the class as a slash command
             await bot.load_extension(f'app.commands.infos.{file_name[:-3]}')
 
 
 async def main():
     await load()
+
+    # Starts the bot using the TOKEN inside .env file
     await bot.start(config.get("TOKEN"))
 
 
