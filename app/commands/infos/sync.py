34767c6249c1479a0919e3e7b3bc9733d8e5b42a
Vinicius Santos <viniciuscarvalhomine@gmail.com>
2022-12-15 19:12:03 -0300

    Change the sync command to a separeted class

---
+++ app/commands/infos/sync.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+
+from app.utils.condition_utils import ConditionUtils
+from app.utils.date_time_utils import get_current_time
+
+
+class Sync(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        @bot.command(name="sync")
+        async def sync(ctx):
+            # Check if the author is me (zvinniie#0484)
+            if ctx.author.id == 240216596075773952:
+
+                # Check if it's not syncing
+                if not ConditionUtils.is_syncing:
+                    ConditionUtils.is_syncing = True
+
+                    synced = await bot.tree.sync()
+
+                    print(f"{get_current_time()} :: Synced {len(synced)} slash commands!")
+                    await ctx.send(f"👍 **`{len(synced)}` slash commands synced!**")
+
+                    ConditionUtils.is_syncing = False
+
+
+async def setup(self):
+    # Adding the class as a slash command
+    await self.add_cog(Sync(self))
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import asyncio
 import os
-from app.utils.condition_utils import ConditionUtils
 from app.utils.date_time_utils import get_current_time
-
 import discord
 from discord.ext import commands
-
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
+token = config.get("TOKEN")
 
 intents = discord.Intents.all()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=".", intents=intents)
+bot = commands.Bot(command_prefix="--", intents=intents)
 client = discord.Client(intents=intents)
 
 
@@ -31,28 +29,11 @@ async def load():
             await bot.load_extension(f'app.commands.infos.{file_name[:-3]}')
 
 
-@bot.command(name="sync")
-async def sync(ctx):
-    # Check if the author is mine (zvinniie#0484)
-    if ctx.author.id == 240216596075773952:
-
-        # Check if it's not syncing
-        if not ConditionUtils.is_syncing:
-            ConditionUtils.is_syncing = True
-
-            synced = await bot.tree.sync()
-
-            print(f"{get_current_time()} :: Synced {len(synced)} slash commands!")
-            await ctx.send(f"👍 **`{len(synced)}` slash commands synced!**")
-
-            ConditionUtils.is_syncing = False
-
-
 async def main():
     await load()
 
     # Starts the bot using the TOKEN inside .env file
-    await bot.start(config.get("TOKEN"))
+    await bot.start(token)
 
 
 asyncio.run(main())
