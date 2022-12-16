1fc11d4d3733b5e2d01eed63dc84180d288a3f1f
Vinicius Carvalho dos Santos <vinicius.santos@ibratan.com.br>
2022-12-16 10:27:24 -0300

    Added another role when member is a bot

--- app/events/on_member_join.py
+++ app/events/on_member_join.py
@@ -10,10 +10,18 @@ class OnMemberJoin(commands.Cog):
         @bot.event
         async def on_member_join(member: discord.Member):
             guild_id = int(config.get("GUILD_ID_ADD_MEMBER"))
-            role_id = int(config.get("ROLE_ID_ADD_MEMBER"))
 
             # Checking if the guild id is the same of the support guild's id
             if member.guild.id == guild_id:
+
+                # Checking if member is bot
+                if member.bot:
+                    # Specific role for bots
+                    role_id = int(config.get("BOT_ROLE_ID_ADD_MEMBER"))
+                else:
+                    # Specific role for non bots
+                    role_id = int(config.get("ROLE_ID_ADD_MEMBER"))
+
                 # Looping by the guild roles
                 for role in member.guild.roles:
                     # if the role id is the same of the support guild's role id
