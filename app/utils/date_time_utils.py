e4c087ada6fd3a288a60a0744aff5720cff82cf3
Vinicius Santos <viniciuscarvalhomine@gmail.com>
2022-12-19 18:51:49 -0300

    Refactoring envs var, sync command and date time utils (#5)
    * Refactoring envs var, sync command and date time utils
    
    * removing conflicts

--- app/utils/date_time_utils.py
+++ app/utils/date_time_utils.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
 
-def get_current_time() -> str:
-    return datetime.now().strftime('%H:%M:%S')
+class DateTimeUtils:
+
+    @staticmethod
+    def get_current_time() -> str:
+        return datetime.now().strftime('%H:%M:%S')
